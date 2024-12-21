509b2ab98ba8b0e8e763bf2efe456a2de8d7ed2e
Dinara231559 <153474014+Dinara231559@users.noreply.github.com>
2024-12-22 04:00:36 +0500

    Initial setup for Blockchain

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from blockchain import Blockchain
+
+if __name__ == "__main__":
+    blockchain = Blockchain()
+
+    transactions1 = [
+        "Alice->Bob->10",
+        "Bob->Charlie->20",
+        "Charlie->David->30",
+        "David->Eve->40",
+        "Eve->Frank->50",
+        "Frank->Grace->60",
+        "Grace->Hannah->70",
+        "Hannah->Ivy->80",
+        "Ivy->Jack->90",
+        "Jack->Kathy->100"
+    ]
+    blockchain.add_block(transactions1)
 
