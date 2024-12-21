0b29ec4b0702690aedeb2934824176d99c1fa567
aiswlz <153474803+aiswlz@users.noreply.github.com>
2024-12-22 03:26:11 +0500

    Add Compute Hash

--- block.py
+++ block.py
@@ -1,8 +1,14 @@
+from sha256 import hash
+
 class Block:
     def __init__(self, previous_hash, timestamp, transactions):
-        """Initialize the block with basic attributes."""
         self.previous_hash = previous_hash
         self.timestamp = timestamp
         self.transactions = transactions
         self.nonce = 0
         self.hash = None
+        
+    def compute_hash(self):
+        """Compute the SHA-256 hash of the block."""
+        block_string = f"{self.previous_hash}{self.timestamp}{self.transactions}{self.nonce}"
+        return hash(block_string)
