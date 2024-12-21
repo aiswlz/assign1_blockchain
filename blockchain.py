678c3b753f6468d79d95b296e5b3753e0284d879
Dinara231559 <153474014+Dinara231559@users.noreply.github.com>
2024-12-22 03:47:36 +0500

    Adding add_block and validate_blockchain functions

--- blockchain.py
+++ blockchain.py
@@ -10,3 +10,19 @@ class Blockchain:
     def create_genesis_block(self):
         genesis_block = Block("0", int(time.time()), [])
         self.chain.append(genesis_block)
+
+    def add_block(self, transactions):
+        last_block = self.chain[-1]
+        new_block = Block(last_block.hash, int(time.time()), transactions)
+        new_block.mine_block(self.difficulty)
+        self.chain.append(new_block)
+
+    def validate_blockchain(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+            if current_block.hash != previous_block.hash:  
+                return False
+            if not current_block.hash.startswith("0" * self.difficulty):
+                return False
+        return True
