5740cf2193ce3e7448514b4c48bcbf6789a1b82e
aiswlz <153474803+aiswlz@users.noreply.github.com>
2024-12-22 03:27:17 +0500

    Add Mining

--- block.py
+++ block.py
@@ -12,3 +12,9 @@ class Block:
         """Compute the SHA-256 hash of the block."""
         block_string = f"{self.previous_hash}{self.timestamp}{self.transactions}{self.nonce}"
         return hash(block_string)
+        
+    def mine_block(self, difficulty):
+        target = "0" * difficulty
+        while not self.hash or not self.hash.startswith(target):
+            self.nonce += 1
+            self.hash = self.compute_hash()
