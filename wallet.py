87601fa9b85dcf53bc1653b900cc90951360c1b8
Dinara231559 <153474014+Dinara231559@users.noreply.github.com>
2025-01-04 02:45:09 +0500

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -1,1 +1,15 @@
+from rsa import sign
+# === Wallet ===
+def create_transaction(wallet_private_key, sender_public_key, receiver_public_key, sender, receiver, amount):
+    document = f"{sender}->{receiver}->{amount}"
+    signature = sign(wallet_private_key, document)
+    return {
+        'sender_public_key': sender_public_key,
+        'receiver_public_key': receiver_public_key,
+        'sender': sender,
+        'receiver': receiver,
+        'amount': amount,
+        'document': document,
+        'signature': signature
+    }
 
