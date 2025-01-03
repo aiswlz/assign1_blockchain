c3de7aa022206cfb25c433a1c4d8b279eb859285
DaniyaSeitzhaparova <153476221+DaniyaSeitzhaparova@users.noreply.github.com>
2025-01-04 00:11:59 +0500

    rsa.py added and generate_keys function created

---
+++ rsa.py
@@ -0,0 +1,14 @@
+from random import randint, getrandbits
+from hash import hash
+
+def generate_keys(bits=512):
+    def gcd(a, b):
+        while b:
+            a, b = b, a % b
+        return a
+
+
+
+
+
+
