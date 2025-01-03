d54ada80baa34bf0b248cbf77df9c1118fcef04b
aiswlz <153474803+aiswlz@users.noreply.github.com>
2025-01-04 02:38:05 +0500

    Add database integration for transactions

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 import time
 from block import Block
 from rsa import generate_keys,verify
+import json
 
 class Blockchain:
     def __init__(self):
@@ -10,6 +11,7 @@ class Blockchain:
 
     def create_genesis_block(self):
         genesis_block = Block("0", int(time.time()), [])
+        genesis_block.mine_block(self.difficulty)
         self.chain.append(genesis_block)
 
     def add_block(self, transactions):
@@ -29,22 +31,24 @@ class Blockchain:
             if not current_block.hash.startswith("0" * self.difficulty):
                 return False
         return True
-        
+
+# === Blockchain Enhancements ===
 class EnhancedBlockchain(Blockchain):
     def __init__(self):
         super().__init__()
         self.wallet_private_key, self.wallet_public_key = generate_keys()
+
     def verify_transaction(self, transaction):
         try:
             sender_pub_key = transaction['sender_public_key']
             document = transaction['document']
             signature = transaction['signature']
-    
+
             if not verify(sender_pub_key, document, signature):
                 raise ValueError("Signature is wrong")
             if document != f"{transaction['sender']}->{transaction['receiver']}->{transaction['amount']}":
                 raise ValueError("Document is wrong")
-    
+
             return True
         except Exception as e:
             print(e)
@@ -54,3 +58,16 @@ class EnhancedBlockchain(Blockchain):
         if self.verify_transaction(transaction):
             self.add_block([transaction])
 
+    def retrieve_transactions_from_db(self, filename="transactions.txt"):
+        try:
+            with open(filename, "r") as file:
+                transactions = json.load(file)
+            return transactions
+        except FileNotFoundError:
+            return []
+
+    def save_transaction_to_db(self, transaction, filename="transactions.txt"):
+        transactions = self.retrieve_transactions_from_db(filename)
+        transactions.append(transaction)
+        with open(filename, "w") as file:
+            json.dump(transactions, file, indent=4)
